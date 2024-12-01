cb52b1f152af7759f21fc66a72a2190094f39be6
Lazuli <34384401+Clean-Hands@users.noreply.github.com>
2024-12-01 16:20:23 -0600

    second attempt; fresh install

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
-import React from 'react';
-import { Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { isLoaded } from 'react-redux-firebase';
+import logo from './logo.svg';
+import './App.css';
 
-import CardEditor from './CardEditor.js';
-import CardViewer from './CardViewer.js';
-import Homepage from './Homepage.js';
-import PageRegister from './PageRegister.js';
-import PageLogin from './PageLogin.js';
-import PageProfile from './PageProfile.js';
-
-const App = props => {
-  if (!isLoaded(props.auth, props.profile)) {
-    return <div>Authentication loading...</div>
-  }
-
-  return(
-  <Switch>
-    {/* <Route exact path="/">
-      <Homepage />
-    </Route> */}
-    <Route exact path="/register">
-      <PageRegister/>
-    </Route>
-    <Route exact path="/login">
-      <PageLogin/>
-    </Route>
-    <Route>
-      <div>Page not found</div>
-    </Route>
-  </Switch>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
-
-const mapStateToProps = state => {
-  return {auth: state.firebase.auth, profile: state.firebase.profile};
 }
 
-export default connect(mapStateToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,59 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
-import firebase from "firebase/app";
-import 'firebase/database';
-import 'firebase/auth';
+import reportWebVitals from './reportWebVitals';
 
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import { ReactReduxFirebaseProvider, firebaseReducer } from 'react-redux-firebase';
-
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCoJhrMeILouXe88-URbvVLcIdM2IjpCUY",
-  authDomain: "cookie-survey.firebaseapp.com",
-  databaseURL: "https://cookie-survey-default-rtdb.firebaseio.com",
-  projectId: "cookie-survey",
-  storageBucket: "cookie-survey.appspot.com",
-  messagingSenderId: "968974432555",
-  appId: "1:968974432555:web:06fdc5669b9313c64f2d6e"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const rootReducer = combineReducers({
-  firebase: firebaseReducer
-  // firestore: firestoreReducer // <- needed if using firestore
-})
-
-// Create store with reducers and initial state
-const store = createStore(rootReducer, composeWithDevTools())
-
-// react-redux-firebase config
-const rrfConfig = {
-  userProfile: 'users',
-  preserveOnLogout: ['names'],
-  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-  // enableClaims: true // Get custom claims along with the profile
-}
-
-const rrfProps = {
-  firebase,
-  config: rrfConfig,
-  dispatch: store.dispatch
-  // createFirestoreInstance // <- needed if using firestore
-}
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReactReduxFirebaseProvider>
-  </Provider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
